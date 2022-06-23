e345f62026bb62a178e6a22923a02f868985916d
Austin Langdon <alangdon93@gmail.com>
2022-06-23 12:57:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,5 +28,5 @@ ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
+echo "Setting up GitHub Personal Access Token."
 export GITHUB_TOKEN=$PAT
-echo "GitHub Personal Access Token set"
